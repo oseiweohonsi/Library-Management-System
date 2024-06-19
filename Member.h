0d9be8a129d7dd3ccf6aef691a7235d7484adb19
Oseiwe Ohonsi <oseiweohonsi@gmail.com>
2024-06-18 23:03:37 -0400

    added member derived class of user

---
+++ Member.h
@@ -0,0 +1,8 @@
+#IFNDEF MEMBER
+#DEFINE MEMBER
+class Member
+{
+	private:
+		unsigned long long int userEcard;
+};
+#endif
---
+++ User.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+class Users
+{
+	private:
+		std::string name;
+		std::string userID;
+		std::string address;
+		std::string phone;
+		std::string email;
+		
+	public:
+			
+		
+};
